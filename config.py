66d7d7672f2b4a328f1f72eafaa4b9d96cb12744
JorgeDDuran <jorgerena3@gmail.com>
2024-10-06 10:26:10 -0500

    limpiar rama

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/jddur/OneDrive/Desktop/Opcion de grado/modulos/instance/diccionario.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Opcional, pero recomendado
