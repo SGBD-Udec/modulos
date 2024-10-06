5724c0357a86dd862cc3da02b41ec1fbe4cdc0e1
JorgeDDuran <jorgerena3@gmail.com>
2024-10-06 09:59:23 -0500

    prueba

--- modulo_ddl_dml/utils.py
+++
@@ -1,5 +0,0 @@
-# instance/modulo_ddl_dml/utils.py
-def validar_nombre(nombre):
-    """Valida que el nombre no esté vacío."""
-    if not nombre:
-        raise ValueError("El nombre no puede estar vacío.")
--- modulo_diccionario/services.py
+++
@@ -1,5 +0,0 @@
-# backend/modulo_diccionario/services.py
-from .models import Tabla
-
-def obtener_tabla_por_id(tabla_id):
-    return Tabla.query.filter_by(id=tabla_id).first()
