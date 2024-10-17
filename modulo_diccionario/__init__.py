58069b1463702f3e9d318240247f0fb55d9f3bef
JorgeDDuran <jorgerena3@gmail.com>
2024-10-17 14:57:20 -0500

    Borrar todo el contenido de main

--- modulo_diccionario/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from .views import modulo_diccionario
-# Crear el Blueprint
-modulo_diccionario = Blueprint('modulo_diccionario', __name__)
-
-# Importar las vistas para evitar problemas de importación circular
-from .views import *
