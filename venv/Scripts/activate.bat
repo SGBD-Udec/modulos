3447f421673a37b704d01b2174c105bdc4ec3d04
JorgeDDuran <jorgerena3@gmail.com>
2024-10-19 13:35:41 -0500

    ajuste diccionario

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Adrian0\Documents\GitHub\modulos\venv
+set VIRTUAL_ENV=C:\Users\jddur\OneDrive\Desktop\Opcion de grado\modulos\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
